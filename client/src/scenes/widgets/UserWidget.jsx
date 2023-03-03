03599090a482f53ff8dca6d82db90debc47ae0e8
Kimberly Marcelin Nathan A <82994804+Kimberly-Marcelin-Nathan@users.noreply.github.com>
2023-03-04 00:15:32 +0530

    Update UserWidget.jsx

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -54,7 +54,7 @@ import {
         <FlexBetween
           gap="0.5rem"
           pb="1.1rem"
-          onClick={() => navigate(`profile/${userId}`)}
+          onClick={() => navigate(`/profile/${userId}`)}
         >
           <FlexBetween gap="1rem">
             <UserImage image={picturePath} />
