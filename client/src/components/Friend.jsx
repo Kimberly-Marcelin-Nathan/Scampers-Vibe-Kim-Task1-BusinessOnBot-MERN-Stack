5c7434e46564a1a21f1f4920918b2921d36ce438
Kimberly Marcelin Nathan A <82994804+Kimberly-Marcelin-Nathan@users.noreply.github.com>
2023-03-22 15:31:32 +0530

    Update Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -3,6 +3,8 @@ import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setFriends } from "state";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import FlexBetween from "./FlexBetween";
 import UserImage from "./UserImage";
 
